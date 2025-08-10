29f715b205572823e9dc7d0d2a6bd49518ff4fc7
Berk Dönmez <220366455+berkdnmz@users.noreply.github.com>
2025-08-10 18:27:43 +0300

    Read words from CSV file instead of hardcoded list.

--- word_manager.py
+++ word_manager.py
@@ -1,20 +1,14 @@
+import csv
 import random
 
 class WordManager():
     def __init__(self):
-        self.words = {
-            "apple": "elma",
-            "book": "kitap",
-            "banana": "muz",
-            "door": "kapı",
-            "window": "pencere",
-            "dog": "köpek",
-            "car": "araba",
-            "house": "ev",
-            "sun": "güneş",
-            "pencil": "kalem"
+        #Read the word list file
+        self.file_path = 'data/words.csv'
 
-        }
+        with open(self.file_path, 'r', encoding='utf-8', newline='') as words_file:
+            self.words = dict(csv.reader(words_file))
+
 
     def select_word(self, count):
         return random.sample(list(self.words.keys()), k=count)
